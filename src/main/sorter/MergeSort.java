84ab4bac45fbeca0ece010a0e36ee932508a5a97
Alberto Claudino dos Santos Silva Filho <albertocssf@gmail.com>
2024-08-15 15:35:18 -0300

    implementando MergeSort

--- src/main/sorter/MergeSort.java
+++ src/main/sorter/MergeSort.java
@@ -2,13 +2,40 @@ package main.sorter;
 
 public class MergeSort implements Sorter {
 
+
     @Override
     public int[] sort(int[] values) {
-        // mergesort implementation
-        return values;
+        if(values.length <= 1){
+            return values;
+        }
+
+        int mid = values.length/2;
+        int[] left = new int[mid];
+        int[] right = new int[values.length - mid];
+
+        left = sort(left);
+        right = sort(right);
+
+        return merge(left, right);
     }
 
     public int[] merge(int[] left, int[] right) {
-        return null;
+        int i = 0, j = 0;
+        int tamanhoEsquerdo = left.length;
+        int tamanhoDireito = right.length;
+        int k = tamanhoEsquerdo + tamanhoDireito;
+        int[] arrayNovo = new int[k];
+
+        while(i < tamanhoEsquerdo && j < tamanhoDireito){
+            if(right[i] <= left[j]){
+                arrayNovo[k] = right[i];
+                i++;
+            }else{
+                arrayNovo[k] = left[j];
+                j++;
+            }
+            k++;
+        }
+        return arrayNovo;
     }
 }
